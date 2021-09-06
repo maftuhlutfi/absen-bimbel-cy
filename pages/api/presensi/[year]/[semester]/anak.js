c680617dce2dd01e1269f7f6cc5d97c9ef67e168
maftuhlutfi <59835501+maftuhlutfi@users.noreply.github.com>
2021-09-06 14:19:57 +0700

    change link

--- pages/api/presensi/[year]/[semester]/anak.js
+++ pages/api/presensi/[year]/[semester]/anak.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req, res) {
     const host = req.rawHeaders[1]
     const {year, semester} = req.query
-    const presensiRes = await axios.get(`/api/presensi/${year}/${semester}`)
+    const presensiRes = await axios.get(`${host.includes('localhost') ? 'http' : 'https'}://${host}/api/presensi/${year}/${semester}`)
     const presensiData = await presensiRes.data
     res.send(presensiData.map(d => d.name))
 }
