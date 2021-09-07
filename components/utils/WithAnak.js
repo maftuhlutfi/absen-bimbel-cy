9e46ebe7428079613ab2ae5ebed47f3cb1d968d8
unknown <playmakernyomlo@gmail.com>
2021-09-07 12:32:44 +0700

    change theme color manifest

--- components/utils/WithAnak.js
+++ components/utils/WithAnak.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import { useRouter } from "next/router"
 import { useContext, useEffect } from "react"
 import { DataContext } from "../Context"
+import Spinner from "../shared/Spinner"
 
 const WithAnak = (Component) => {
     function Protected({...otherProps}) {
@@ -10,16 +11,8 @@ const WithAnak = (Component) => {
 
         if (loading) {
             return (
-                <div className='z-50 w-screen h-screen flex items-center justify-center bg-white fixed top-0 left-0'>
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={{ margin: 'auto', background: 'rgb(255, 255, 255)', display: 'block', shapeRendering: 'auto' }} width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-                        <circle cx={50} cy={50} r={0} fill="none" stroke="#222222" strokeWidth={2}>
-                            <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="0s" />
-                            <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="0s" />
-                        </circle><circle cx={50} cy={50} r={0} fill="none" stroke="#9a9a9a" strokeWidth={2}>
-                            <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.5s" />
-                            <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.5s" />
-                        </circle>
-                    </svg>
+                <div className='h-screen bg-light-blue w-full flex items-center justify-center fixed left-0 top-0 z-20'>
+                    <Spinner />
                 </div>
             )
         }
