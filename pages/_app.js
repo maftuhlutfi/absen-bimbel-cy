99476eda1915207f3185e27798758308d0690844
unknown <playmakernyomlo@gmail.com>
2021-09-07 11:55:29 +0700

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
+import '../styles/nprogress.css'; 
 
 import Header from '../components/shared/Header'
 import AllContextProvider from '../components/Context'
+import Router from 'next/router';
+import NProgress from 'nprogress'
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
